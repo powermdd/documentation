1462f628e6f1e858428ebe185700c83d0ce375aa
powermdd <powermdd@users.noreply.github.com>
2023-09-18 06:28:15 +0000

    deploy: cf0e9ce8c9963162a91a4ace4f8077b11fd05d87

---
+++ assets/DatabaseBoc.html-964e0c09.js
@@ -0,0 +1,1 @@
+import{_ as a,o as t,c,d as e,f as s}from"./app-db59dfda.js";const o={},r=e("h1",{id:"database-boc",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#database-boc","aria-hidden":"true"},"#"),s(" Database BOC")],-1),d=[r];function n(_,i){return t(),c("div",null,d)}const l=a(o,[["render",n],["__file","DatabaseBoc.html.vue"]]);export{l as default};
