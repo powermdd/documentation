14a6cc66e171be602c4f94dcadddbeda36a38984
powermdd <powermdd@users.noreply.github.com>
2023-09-21 08:22:26 +0000

    deploy: 29c0ab5c13b1a7284c064e4b98e0c344fd666f14

--- assets/ControlBoc.html-43f39f5e.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as t,c,d as e,f as r}from"./app-0797e269.js";const n={},a=e("h1",{id:"control-boc",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#control-boc","aria-hidden":"true"},"#"),r(" Control BOC")],-1),s=[a];function _(l,d){return t(),c("div",null,s)}const h=o(n,[["render",_],["__file","ControlBoc.html.vue"]]);export{h as default};
