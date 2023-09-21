01b70ab17f084cafa9aded25a29e1ed2a11bdb76
powermdd <powermdd@users.noreply.github.com>
2023-09-21 00:47:25 +0000

    deploy: 23e17b736ce4a3233c7f7715b1e449d424e107d3

--- assets/DatabaseBoc.html-a5ad2a87.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-4fabeb4a","path":"/documentation/service-model/DatabaseBoc.html","title":"Database BOC","lang":"ko-KR","frontmatter":{},"headers":[],"git":{"updatedTime":1694413966000,"contributors":[{"name":"User","email":"User@example.com","commits":3}]},"filePathRelative":"documentation/service-model/DatabaseBoc.md"}');export{e as data};
