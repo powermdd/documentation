9e6f7ad43feb97fa8a1ba142f52caba52d516f3a
powermdd <powermdd@users.noreply.github.com>
2023-10-24 02:49:12 +0000

    deploy: e63a1934e3a1fe7b9192a5b1d4a70b1907d92022

--- assets/InterfaceBoc.html-23990dbf.js
+++
@@ -1,1 +0,0 @@
-import{_ as c,o as t,c as a,d as e,f as r}from"./app-58a3ca58.js";const o={},n=e("h1",{id:"interface-boc",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#interface-boc","aria-hidden":"true"},"#"),r(" Interface BOC")],-1),s=[n];function _(d,f){return t(),a("div",null,s)}const h=c(o,[["render",_],["__file","InterfaceBoc.html.vue"]]);export{h as default};
